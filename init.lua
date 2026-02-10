c56dba7f51b32039672d7a3a33122031a919fff6
uberrice <uberrice@pm.me>
2026-02-10 09:55:20 +0100

    add more extras

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.keymap.set("n", "<S-CR>", "m`o<Esc>``", { noremap = true, silent = true, desc = "Blank line below" })
+vim.keymap.set("n", "<A-CR>", "m`O<Esc>``", { noremap = true, silent = true, desc = "Blank line above" })
