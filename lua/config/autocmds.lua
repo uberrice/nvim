440f9f1fd932290c2bec6aa6a972d4f9d436166b
uberrice <uberrice@pm.me>
2026-02-27 17:37:01 +0100

    add wrapping for text file types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,10 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- vim.api.nvim_create_autocmd("VimEnter", {
---   callback = function()
---     vim.defer_fn(function()
---       vim.cmd("silent! Copilot disable")
---     end, 100) -- small delay helps avoid timing issues
---   end,
--- })
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
