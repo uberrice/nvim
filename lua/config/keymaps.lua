e9302fa34d10598fab0a573d86d7c00547662b16
uberrice <uberrice@pm.me>
2026-09-02 08:29:31 +0200

    remove sshfs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,24 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- remote-sshfs
-local api = require("remote-sshfs.api")
 local wk = require("which-key")
-
-vim.keymap.set("n", "<leader>rc", api.connect, { desc = "Connect to remote host" })
-vim.keymap.set("n", "<leader>rd", api.disconnect, { desc = "Disconnect from remote host" })
-vim.keymap.set("n", "<leader>re", api.edit, { desc = "Edit remote hosts" })
 
 -- (optional) Override telescope find_files to make dynamic based on if connected to host
 local builtin = require("telescope.builtin")
-local connections = require("remote-sshfs.connections")
 
 vim.keymap.set("n", "<leader>ff", function()
-  if connections.is_connected() then
-    api.find_files()
-  else
-    builtin.find_files()
-  end
+  builtin.find_files()
 end, { desc = "Find Files (Root Dir)" })
 
 wk.add({
