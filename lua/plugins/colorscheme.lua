25b7dd43a0e9923e1bdf8a7685d0696b36e120af
uberrice <uberrice@pm.me>
2026-09-03 20:44:14 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
   -- add catppuccin
   { "catppuccin" },
+  { "rebelot/kanagawa.nvim" },
 
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "kanagawa-dragon",
     },
   },
 }
